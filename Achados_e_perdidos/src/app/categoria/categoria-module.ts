4e833b86170c276e2c27a2b542595061c66063eb
campeseriquepi-design <campese.rique.pi@gmail.com>
2026-08-28 01:41:59 +0000

    save 2

--- Achados_e_perdidos/src/app/categoria/categoria-module.ts
+++ Achados_e_perdidos/src/app/categoria/categoria-module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CategoriaRoutingModule } from './categoria-routing-module';
+import { Cadastro } from './cadastro/cadastro';
+import { Listagem } from './listagem/listagem';
 
 @NgModule({
-  declarations: [],
+  declarations: [Cadastro, Listagem],
   imports: [CommonModule, CategoriaRoutingModule],
 })
 export class CategoriaModule {}
