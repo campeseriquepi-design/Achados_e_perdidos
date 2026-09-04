4a64cbcc2f2cb7eee0ebcfc3921fd241da1676b9
campeseriquepi-design <campese.rique.pi@gmail.com>
2026-09-04 01:25:27 +0000

    ex7

--- Achados_e_perdidos/src/app/app-module.ts
+++ Achados_e_perdidos/src/app/app-module.ts
@@ -10,10 +10,11 @@ import { Ex3 } from './lista-ex/ex3/ex3';
 import { Ex4 } from './lista-ex/ex4/ex4';
 import { Ex5 } from './lista-ex/ex5/ex5';
 import { Ex6 } from './lista-ex/ex6/ex6';
+import { Ex7 } from './lista-ex/ex7/ex7';
 
 
 @NgModule({
-  declarations: [App, Ex1, Ex2, Ex3, Ex4, Ex5, Ex6],
+  declarations: [App, Ex1, Ex2, Ex3, Ex4, Ex5, Ex6, Ex7],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
